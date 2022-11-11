9b4edc2e1d8b4ae484d23bf14824665c1f73d893
alli-Codes <allisonevans081@gmail.com>
2022-11-11 11:28:15 +0100

    Updating the TopBanner component with another gif image

--- src/components/TopBanner.vue
+++ src/components/TopBanner.vue
@@ -1,36 +1,39 @@
 <template>
-    <footer class="top-banner h-[85px] w-full flex flex-col justify-evenly items-stretch">
-        <div class="bg-[#ec4320] flex-1 flex justify-center">
-            <a href="#"><img src="images/top-banner.gif" alt="" class=""></a>
+  <footer
+    class="top-banner h-[85px] w-full flex flex-col justify-evenly items-stretch"
+  >
+    <div class="bg-black flex-1 flex justify-center">
+      <a href="#"
+        ><img src="images/jumia_black/top-banner.gif" alt="" class=""
+      /></a>
+    </div>
+    <div class="bg-gray-100 h-8 flex justify-evenly">
+      <div class="w-3/4 flex justify-between items-center">
+        <a href="#" class="text-sm flex items-center">
+          <img src="/images/star-logo.png" class="w-4" alt="" />
+          <p class="pl-2 text-[#f68b1e] hover:underline">Sell on Jumia</p>
+        </a>
+        <div class="flex-1 flex justify-center items-center">
+          <img src="/images/logo.png" class="w-12" alt="" />
+          <ul
+            class="w-1/2 flex justify-evenly text-sm font-bold text-gray-300 links"
+          >
+            <li><a href="#">PAY</a></li>
+            <li><a href="#">FOOD</a></li>
+            <li><a href="#">PARTY</a></li>
+            <li><a href="#">PRIME</a></li>
+            <li><a href="#">LOGISTICS</a></li>
+          </ul>
         </div>
-        <div class="bg-gray-100 h-8 flex justify-evenly">
-            <div class="w-3/4 flex justify-between items-center">
-                <a href="#" class=" text-sm flex items-center ">
-                    <img src="/images/star-logo.png" class="w-4 " alt="">
-                    <p class="pl-2 text-[#f68b1e] hover:underline">Sell on Jumia</p>
-                </a>
-                <div class="flex-1 flex justify-center items-center">
-                    <img src="/images/logo.png" class="w-12" alt="">
-                    <ul class="w-1/2 flex justify-evenly text-sm font-bold text-gray-300 links">
-                        <li><a href="#">PAY</a></li>
-                        <li><a href="#">FOOD</a></li>
-                        <li><a href="#">PARTY</a></li>
-                        <li><a href="#">PRIME</a></li>
-                        <li><a href="#">LOGISTICS</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+      </div>
+    </div>
 
-        <div>
-
-        </div>
-    </footer>
+    <div></div>
+  </footer>
 </template>
 
 <style scoped>
-
-    .links a:hover{
-        color: #f68b1e
-    }
+.links a:hover {
+  color: #f68b1e;
+}
 </style>
